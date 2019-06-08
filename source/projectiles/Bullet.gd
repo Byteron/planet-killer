b4cc5d17305d2fcc62166d2ead2939caa13aa819
Aaron Winter <winter.aaron93@gmail.com>
2019-06-08 14:26:31 +0200

    enemie bullet game

--- source/projectiles/Bullet.gd
+++ source/projectiles/Bullet.gd
@@ -4,3 +4,10 @@ export var speed := 400
 
 func _physics_process(delta: float) -> void:
 	global_position.y -= speed * delta
+
+func _on_Bullet_area_entered(area: Area2D) -> void:
+	queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
