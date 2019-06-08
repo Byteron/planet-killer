9ac6cc0a62ad5a000a5256e56e80cb9712504608
Aaron Winter <winter.aaron93@gmail.com>
2019-06-08 23:36:27 +0200

    another background layer

--- source/game/Background.gd
+++ source/game/Background.gd
@@ -2,9 +2,14 @@ extends ParallaxBackground
 
 onready var layers = [
 	$ParallaxLayer,
-	$ParallaxLayer2
+	$ParallaxLayer2,
+	$ParallaxLayer3
 ]
 
+func _ready() -> void:
+	layers[1].motion_offset.y += 200
+
 func _process(delta: float) -> void:
-	layers[0].motion_offset.y += 500 * delta
-	layers[1].motion_offset.y += 800 * delta
+	layers[0].motion_offset.y += 200 * delta
+	layers[1].motion_offset.y += 240 * delta
+	layers[2].motion_offset.y += 880 * delta
