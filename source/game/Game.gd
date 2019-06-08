8f04c3d23583802520785f0c3183d3e670fd0cbb
Aaron Winter <winter.aaron93@gmail.com>
2019-06-08 17:51:51 +0200

    game background

--- source/game/Game.gd
+++ source/game/Game.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+onready var layers = [
+	$ParallaxBackground/ParallaxLayer,
+	$ParallaxBackground/ParallaxLayer2
+]
+
+func _process(delta: float) -> void:
+	layers[0].motion_offset.y += 500 * delta
+	layers[1].motion_offset.y += 800 * delta
+
 
 func _on_Player_shot_fired(percentage):
 	$Camera2D.shake(0.3, 40 * percentage, 10 * percentage);
