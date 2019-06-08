f6343498a2c5697dcdd96b603cce2a6ed659796f
Aaron Winter <winter.aaron93@gmail.com>
2019-06-08 18:02:37 +0200

    stuff

--- source/game/Game.gd
+++ source/game/Game.gd
@@ -1,18 +1,16 @@
 extends Node2D
 
-onready var layers = [
-	$ParallaxBackground/ParallaxLayer,
-	$ParallaxBackground/ParallaxLayer2
-]
-
-func _process(delta: float) -> void:
-	layers[0].motion_offset.y += 500 * delta
-	layers[1].motion_offset.y += 800 * delta
-
+onready var spawn_timer := $SpawnTimer as Timer
 
 func _on_Player_shot_fired(percentage):
 	$Camera2D.shake(0.3, 40 * percentage, 10 * percentage);
 
-
 func _on_Player_overheat(timer):
 	$Camera2D.shake(timer, 40, 10);
+
+func spawn_enemy():
+	var enemy = Instance.Enemy()
+	add_child(enemy)
+
+func _on_SpawnTimer_timeout() -> void:
+	spawn_enemy()
