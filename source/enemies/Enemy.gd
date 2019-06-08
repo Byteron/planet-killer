e6eab6c119437239fc39577fca57fd67aee63b42
Aaron Winter <winter.aaron93@gmail.com>
2019-06-08 15:23:55 +0200

    more bullets

--- source/enemies/Enemy.gd
+++ source/enemies/Enemy.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 onready var cannon := $Cannon
 
+var rot := 0.0
+
+func _process(delta: float) -> void:
+	rot += delta * 2
+
 func shoot() -> void:
-	var bullet = Instance.Bullet(cannon.global_position, rotation_degrees, 1600)
+	var bullet = Instance.Bullet(cannon.global_position, rand_range(-rot, rot), 1600)
+	bullet.shooter = self
 	get_parent().add_child(bullet)
 
 func _on_Timer_timeout() -> void:
--- source/projectiles/Bullet.gd
+++ source/projectiles/Bullet.gd
@@ -1,17 +1,24 @@
-extends Area2D
+extends Node2D
 
 var speed = 1000
+var shooter = null
+
+onready var bullet := $Bullet
 
 func _physics_process(delta: float) -> void:
-	global_position.y -= speed * delta
+	bullet.position.y -= speed * delta
 
 func initialize(pos, rot, speed = 1000):
-	self.global_position = pos
-	self.rotation_degrees = rot
-	self.speed = speed
+	position = pos
+	rotation_degrees = rot
+	speed = speed
 
 func _on_Bullet_area_entered(area: Area2D) -> void:
-	print(name, " collided")
+
+	if shooter == area:
+		return
+
+	print(name, " collided with ", area.name)
 	queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
